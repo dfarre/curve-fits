a6010f6dbdaea7b508f9889e816078669d444ee2
coleopter <daniel@naturitas.es>
2019-04-20 16:39:35 +0200

    Fix tests

--- tests/test_curves.py
+++ tests/test_curves.py
@@ -15,7 +15,7 @@ class CurveAlgebraTests(unittest.TestCase):
         w = numpy.array([1, 0, Curve(Polynomial(1, 1))])
         curve = numpy.dot(u - v, w)
 
-        assert repr(curve) == '<Curve: 3 + (-0.5)s*log(s) + (-3.14)/s + (2) + (2)s>'
+        assert repr(curve) == '<Curve: 3 + (-0.5)s·log(s) + (-3.14)/s + (2) + (2)s>'
         assert iround(curve(numpy.array([2.34]))[0], 7) == iround(numpy.array([
             3 - 0.5*(2.34 + 1)*numpy.log(2.34 + 1) - 3.14/2.34 + 2 + 2*2.34])[0], 7)
 
